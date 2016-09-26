1cad41397c1ca8c165d2c689dfcbaa02e48237bb
Jacek Śmietański <jacek.smietanski@ii.uj.edu.pl>
2016-09-26 20:59:45 +0200

    Code review
    1. Simplified some function names
    2. Marked some issues

--- UniTests_PDBe.py
+++ UniTests_PDBe.py
@@ -1,127 +1,148 @@
-import unittest
-from Modules.PDBe_module import *
+from unittest import TestCase
+from PyPDBe.PDBe_module import PyPDBe
 
+#TODO sprawdzac zwracanie konkretnych wartosci
 
-class PDBeTest(unittest.TestCase):
-    def test_get_summary_info(self):
-        self.assertTrue(get_summary_info('2ce3'))
-        self.assertTrue(get_summary_info('2CE3'))
-        self.assertIsNone(get_summary_info('asdasdasd'))
-        self.assertIsNone(get_summary_info('2'))
-        self.assertIsNone(get_summary_info(''))
+class PDBeTest(TestCase):
+
+    def test_get_summary(self):
+        p = PyPDBe()
+        self.assertTrue(p.get_summary('2ce3'))
+        self.assertTrue(p.get_summary('2CE3'))
+        self.assertIsNone(p.get_summary('asdasdasd'))
+        self.assertIsNone(p.get_summary('2'))
+        self.assertIsNone(p.get_summary(''))
 
     def test_get_molecules(self):
-        self.assertTrue(get_molecules('2ce3'))
-        self.assertTrue(get_molecules('2CE3'))
-        self.assertIsNone(get_molecules('asdasdasd'))
-        self.assertIsNone(get_molecules('2'))
-        self.assertIsNone(get_molecules(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_molecules('2ce3'))
+        self.assertTrue(p.get_molecules('2CE3'))
+        self.assertIsNone(p.get_molecules('asdasdasd'))
+        self.assertIsNone(p.get_molecules('2'))
+        self.assertIsNone(p.get_molecules(''))
 
     def test_get_publications(self):
-        self.assertTrue(get_publications('2ce3'))
-        self.assertTrue(get_publications('2CE3'))
-        self.assertIsNone(get_publications('asdasdasd'))
-        self.assertIsNone(get_publications('2'))
-        self.assertIsNone(get_publications(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_publications('2ce3'))
+        self.assertTrue(p.get_publications('2CE3'))
+        self.assertIsNone(p.get_publications('asdasdasd'))
+        self.assertIsNone(p.get_publications('2'))
+        self.assertIsNone(p.get_publications(''))
+
+    # TODO test_get_related_publications
 
     def test_get_experiment(self):
-        self.assertTrue(get_experiment('2ce3'))
-        self.assertTrue(get_experiment('2CE3'))
-        self.assertIsNone(get_experiment('asdasdasd'))
-        self.assertIsNone(get_experiment('2'))
-        self.assertIsNone(get_experiment(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_experiment('2ce3'))
+        self.assertTrue(p.get_experiment('2CE3'))
+        self.assertIsNone(p.get_experiment('asdasdasd'))
+        self.assertIsNone(p.get_experiment('2'))
+        self.assertIsNone(p.get_experiment(''))
 
-    def test_get_NMR_resource(self):
-        self.assertTrue(get_NMR_resource('2k8v'))
-        self.assertTrue(get_NMR_resource('2k7v'))
-        self.assertIsNone(get_NMR_resource('asdasdasd'))
-        self.assertIsNone(get_NMR_resource('2'))
-        self.assertIsNone(get_NMR_resource(''))
+    def test_get_NMR_resources(self):
+        p = PyPDBe()
+        self.assertTrue(p.get_NMR_resources('2k8v'))
+        self.assertTrue(p.get_NMR_resources('2k7v'))
+        self.assertIsNone(p.get_NMR_resources('asdasdasd'))
+        self.assertIsNone(p.get_NMR_resources('2'))
+        self.assertIsNone(p.get_NMR_resources(''))
 
     def test_get_ligands(self):
-        self.assertTrue(get_ligands('1cbs'))
-        self.assertIsNone(get_ligands('asdasdasd'))
-        self.assertIsNone(get_ligands('2'))
-        self.assertIsNone(get_ligands(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_ligands('1cbs'))
+        self.assertIsNone(p.get_ligands('asdasdasd'))
+        self.assertIsNone(p.get_ligands('2'))
+        self.assertIsNone(p.get_ligands(''))
 
     def test_get_modified_residues(self):
-        self.assertTrue(get_modified_residues('4v5j'))
-        self.assertTrue(get_modified_residues('4V5J'))
-        self.assertIsNone(get_modified_residues('asdasdasd'))
-        self.assertIsNone(get_modified_residues('2'))
-        self.assertIsNone(get_modified_residues(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_modified_residues('4v5j'))
+        self.assertTrue(p.get_modified_residues('4V5J'))
+        self.assertIsNone(p.get_modified_residues('asdasdasd'))
+        self.assertIsNone(p.get_modified_residues('2'))
+        self.assertIsNone(p.get_modified_residues(''))
 
     def test_get_mutated_residues(self):
-        self.assertTrue(get_mutated_residues('4v5j'))
-        self.assertTrue(get_mutated_residues('4V5J'))
-        self.assertIsNone(get_mutated_residues('asdasdasd'))
-        self.assertIsNone(get_mutated_residues('2'))
-        self.assertIsNone(get_mutated_residues(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_mutated_residues('4v5j'))
+        self.assertTrue(p.get_mutated_residues('4V5J'))
+        self.assertIsNone(p.get_mutated_residues('asdasdasd'))
+        self.assertIsNone(p.get_mutated_residues('2'))
+        self.assertIsNone(p.get_mutated_residues(''))
+        #TODO check - test fails
 
     def test_get_release_status(self):
-        self.assertTrue(get_release_status('1cbs'))
-        self.assertTrue(get_release_status('1CBS'))
-        self.assertIsNone(get_release_status('asdasdasd'))
-        self.assertIsNone(get_release_status('2'))
-        self.assertIsNone(get_release_status(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_release_status('1cbs'))
+        self.assertTrue(p.get_release_status('1CBS'))
+        self.assertIsNone(p.get_release_status('asdasdasd'))
+        self.assertIsNone(p.get_release_status('2'))
+        self.assertIsNone(p.get_release_status(''))
 
     def test_get_observed_ranges(self):
-        self.assertTrue(get_observed_ranges('1cbs'))
-        self.assertTrue(get_observed_ranges('1CBS'))
-        self.assertIsNone(get_observed_ranges('asdasdasd'))
-        self.assertIsNone(get_observed_ranges('2'))
-        self.assertIsNone(get_observed_ranges(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_observed_ranges('1cbs'))
+        self.assertTrue(p.get_observed_ranges('1CBS'))
+        self.assertIsNone(p.get_observed_ranges('asdasdasd'))
+        self.assertIsNone(p.get_observed_ranges('2'))
+        self.assertIsNone(p.get_observed_ranges(''))
 
-    def test_get_observed_ranges_in_PDB_chain(self):
-        self.assertTrue(get_observed_ranges_in_PDB_chain('1cbs', 'A'))
-        self.assertTrue(get_observed_ranges_in_PDB_chain('1CBS', 'A'))
-        self.assertIsNone(get_observed_ranges_in_PDB_chain('asdasdasd', 'asdadas'))
-        self.assertIsNone(get_observed_ranges_in_PDB_chain('2', '222'))
-        self.assertIsNone(get_observed_ranges_in_PDB_chain('', ''))
+    def test_get_observed_ranges_in_chain(self):
+        p = PyPDBe()
+        self.assertTrue(p.get_observed_ranges('1cbs', 'A'))
+        self.assertTrue(p.get_observed_ranges('1CBS', 'A'))
+        self.assertIsNone(p.get_observed_ranges('asdasdasd', 'asdadas'))
+        self.assertIsNone(p.get_observed_ranges('2', '222'))
+        self.assertIsNone(p.get_observed_ranges('', ''))
+        # TODO check - test fails
 
     def test_get_secondary_structure(self):
-        self.assertTrue(get_secondary_structure('1cbs'))
-        self.assertTrue(get_secondary_structure('1CBS'))
-        self.assertIsNone(get_secondary_structure('asdasdasd'))
-        self.assertIsNone(get_secondary_structure('2'))
-        self.assertIsNone(get_secondary_structure(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_secondary_structure('1cbs'))
+        self.assertTrue(p.get_secondary_structure('1CBS'))
+        self.assertIsNone(p.get_secondary_structure('asdasdasd'))
+        self.assertIsNone(p.get_secondary_structure('2'))
+        self.assertIsNone(p.get_secondary_structure(''))
 
-    def test_get_list_of_residues_with_modelling_information(self):
-        self.assertTrue(get_list_of_residues_with_modelling_information('1cbs'))
-        self.assertTrue(get_list_of_residues_with_modelling_information('1CBS'))
-        self.assertIsNone(get_list_of_residues_with_modelling_information('asdasdasd'))
-        self.assertIsNone(get_list_of_residues_with_modelling_information('2'))
-        self.assertIsNone(get_list_of_residues_with_modelling_information(''))
+    def test_get_residues(self):
+        p = PyPDBe()
+        self.assertTrue(p.get_residues('1cbs'))
+        self.assertTrue(p.get_residues('1CBS'))
+        self.assertIsNone(p.get_residues('asdasdasd'))
+        self.assertIsNone(p.get_residues('2'))
+        self.assertIsNone(p.get_residues(''))
 
-    def test_get_list_of_residues_with_modelling_information_for_a_particular_PDB_chain(self):
-        self.assertTrue(get_list_of_residues_with_modelling_information_for_a_particular_PDB_chain('1cbs', 'A'))
-        self.assertTrue(get_list_of_residues_with_modelling_information_for_a_particular_PDB_chain('1CBS', 'A'))
-        self.assertIsNone(
-            get_list_of_residues_with_modelling_information_for_a_particular_PDB_chain('asdasdasd', 'asdadas'))
-        self.assertIsNone(get_list_of_residues_with_modelling_information_for_a_particular_PDB_chain('2', '222'))
-        self.assertIsNone(get_list_of_residues_with_modelling_information_for_a_particular_PDB_chain('', ''))
+    def test_get_residues_for_chain(self):
+        p = PyPDBe()
+        self.assertTrue(p.get_residues('1cbs', 'A'))
+        self.assertTrue(p.get_residues('1CBS', 'A'))
+        self.assertIsNone(p.get_residues('asdasdasd', 'asdadas'))
+        self.assertIsNone(p.get_residues('2', '222'))
+        self.assertIsNone(p.get_residues('', ''))
+        #TODO check - test fails
 
     def test_get_binding_sites(self):
-        self.assertTrue(get_binding_sites('1cbs'))
-        self.assertTrue(get_binding_sites('1CBS'))
-        self.assertIsNone(get_binding_sites('asdasdasd'))
-        self.assertIsNone(get_binding_sites('2'))
-        self.assertIsNone(get_binding_sites(''))
+        p = PyPDBe()
+        self.assertTrue(p.get_binding_sites('1cbs'))
+        self.assertTrue(p.get_binding_sites('1CBS'))
+        self.assertIsNone(p.get_binding_sites('asdasdasd'))
+        self.assertIsNone(p.get_binding_sites('2'))
+        self.assertIsNone(p.get_binding_sites(''))
 
-    def test_get_URLs_of_various_files_associated_with_a_PDB_entry(self):
-        self.assertTrue(get_URLs_of_various_files_associated_with_a_PDB_entry('1cbs'))
-        self.assertTrue(get_URLs_of_various_files_associated_with_a_PDB_entry('1CBS'))
-        self.assertIsNone(get_URLs_of_various_files_associated_with_a_PDB_entry('asdasdasd'))
-        self.assertIsNone(get_URLs_of_various_files_associated_with_a_PDB_entry('2'))
-        self.assertIsNone(get_URLs_of_various_files_associated_with_a_PDB_entry(''))
+    def test_get_files(self):
+        p = PyPDBe()
+        self.assertTrue(p.get_files('1cbs'))
+        self.assertTrue(p.get_files('1CBS'))
+        self.assertIsNone(p.get_files('asdasdasd'))
+        self.assertIsNone(p.get_files('2'))
+        self.assertIsNone(p.get_files(''))
 
-    def test_get_ratio_of_observed_residues(self):
-        self.assertTrue(get_ratio_of_observed_residues('1cbs'))
-        self.assertTrue(get_ratio_of_observed_residues('1CBS'))
-        self.assertIsNone(get_ratio_of_observed_residues('asdasdasd'))
-        self.assertIsNone(get_ratio_of_observed_residues('2'))
-        self.assertIsNone(get_ratio_of_observed_residues(''))
+    def test_get_observed_residues_ratio(self):
+        p = PyPDBe()
+        self.assertTrue(p.get_observed_residues_ratio('1cbs'))
+        self.assertTrue(p.get_observed_residues_ratio('1CBS'))
+        self.assertIsNone(p.get_observed_residues_ratio('asdasdasd'))
+        self.assertIsNone(p.get_observed_residues_ratio('2'))
+        self.assertIsNone(p.get_observed_residues_ratio(''))
 
-
-if __name__ == '__main__':
-    unittest.main()
+    # TODO test_get_assembly
